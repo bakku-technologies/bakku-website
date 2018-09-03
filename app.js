02930fb98b0e13a02318d2aaa32c6dd995d88315
MitchGaines <gainesmitch2@gmail.com>
2018-09-03 17:19:30 -0400

    port config update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 let app = express();
-const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -9,6 +8,6 @@ app.get('/', function(req, res) {
 	res.render('pages/index');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at PORT ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running`);
 });
